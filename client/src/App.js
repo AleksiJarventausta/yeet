8cef96b7e6f7874012a70f5f6fea86001cdd3bfa
Maija <maijaaaw@gmail.com>
2020-01-10 17:48:12 +0200

    Added grid imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Grid, Divider } from "semantic-ui-react";
 
 // Utils
 import jwt_decode from "jwt-decode";
@@ -10,35 +11,33 @@ import Login from "./components/auth/Login";
 import "./App.css";
 import Axios from "axios";
 
-
-
 Axios.defaults.baseURL = "http://yeet-yeet.rahtiapp.fi";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.setCurrentUser = this.setCurrentUser.bind(this);
-if (localStorage.jwtTokenTeams) {
-  // Set auth token header auth
-  const token = JSON.parse(localStorage.jwtTokenTeams);
-  setAuthToken(token);
+    if (localStorage.jwtTokenTeams) {
+      // Set auth token header auth
+      const token = JSON.parse(localStorage.jwtTokenTeams);
+      setAuthToken(token);
 
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
+      // Decode token and get user info and exp
+      const decoded = jwt_decode(token);
 
-  // Set user and isAuthenticated
-  this.setCurrentUser(decoded);
+      // Set user and isAuthenticated
+      this.setCurrentUser(decoded);
 
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    this.setCurrentUser(null)
+      // Check for expired token
+      const currentTime = Date.now() / 1000; // to get in milliseconds
+      if (decoded.exp < currentTime) {
+        // Logout user
+        this.setCurrentUser(null);
 
-    // Redirect to login
-    window.location.href = "./";
-  }
-}
+        // Redirect to login
+        window.location.href = "./";
+      }
+    }
   }
   state = {
     user: {},
