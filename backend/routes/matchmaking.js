d99dc14bc457524c8fb9b25bfa79c27a19a8bacf
aksu <aksu.j@hotmail.com>
2020-01-12 12:42:52 +0200

    deleting extra routes.

--- backend/routes/matchmaking.js
+++ backend/routes/matchmaking.js
@@ -17,35 +17,7 @@ router.get(
   }
 );
 
-router.post(
-  "/search",
-  passport.authenticate("jwt", { session: false }),
-  function(req, res) {
-    Post.findOne({ poster: req.user._id }).exec(function(err, post) {
-      if (err) return res.status(400).json({ error: "failed" });
-      if (post) {
-        post.active = req.body.active;
-        post.description = req.body.description;
 
-        post.save(null, function(err, post) {
-          if (err) return res.status(400).json({ error: "failed" });
-          res.status(200).send("update ok");
-        });
-      } else {
-        const newPost = new Post({
-          poster: req.user._id,
-          description: req.body.description,
-          active: true,
-          _id: mongoose.Types.ObjectId()
-        });
-        newPost.save(null, function(err, post) {
-          if (err) return res.status(400).json({ error: "failed" });
-          res.status(200).send("new post ok");
-        });
-      }
-    });
-  }
-);
 
 router.post("/like", passport.authenticate("jwt", { session: false }), function(
   req,
