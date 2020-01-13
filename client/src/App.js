3fb0f01c303ca9b3cde57d13eeb0491ccf5bdc70
petterigit <p-makela@outlook.com>
2020-01-13 14:23:02 +0200

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -88,7 +88,7 @@ class App extends React.Component {
           {/* Header row */}
           <Grid.Row centered>
             <Grid.Column>
-              <Header user={this.state.user} />
+              <Header tab={this.state.currentTab} user={this.state.user} />
             </Grid.Column>
           </Grid.Row>
 
