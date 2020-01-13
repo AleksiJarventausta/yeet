4a388127c5ab0047f9561ee1224d8b47dd515303
aksu <aksu.j@hotmail.com>
2020-01-13 13:49:26 +0200

    user update fix.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,37 @@ var loginValidation = require("../validation/validateLogin");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+function sendJwtToken(res, payload) {
+  jwt.sign(
+    payload,
+    process.env.secretOrKey,
+    {
+      expiresIn: 31556926 // 1 year in seconds
+    },
+    (err, token) => {
+      res.json({
+        success: true,
+        token: "Bearer " + token
+      });
+    }
+  );
+}
+
+function saveUser(res, sendUser) {
+  sendUser
+    .save()
+    .then(user => {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        discord: user.discord,
+        additional: user.additional
+      };
+      sendJwtToken(res, payload);
+    })
+    .catch(err => res.json({ error: "failed to update user info." }));
+}
+
 router.post("/login", function(req, res) {
   const { errors, isValid } = loginValidation(req.body);
 
@@ -31,25 +62,14 @@ router.post("/login", function(req, res) {
         // User matched
         // Create JWT Payload
         const payload = {
+          id: user._id,
           username: user.username,
           discord: user.discord,
-          additional: user.additional,
+          additional: user.additional
         };
 
         // Sign token
-        jwt.sign(
-          payload,
-          process.env.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
+        sendJwtToken(res, payload);
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
@@ -57,17 +77,30 @@ router.post("/login", function(req, res) {
   });
 });
 
-router.post("/update",
+router.post(
+  "/update",
   passport.authenticate("jwt", { session: false }),
   function(req, res, next) {
     req.user.discord = req.body.discord;
     req.user.additional = req.body.additional;
-
-
-
-
+    if (
+      req.body.username !== undefined &&
+      req.body.username !== req.user.username
+    ) {
+      User.findOne({ username: req.body.username }, function(err, user) {
+        if (err) return res.status(400).json({ error: "failed" });
+        if (user) {
+          return res.status(400).json({ username: "Username already in use" });
+        } else {
+          req.user.username = req.body.username;
+          saveUser(res, req.user);
+        }
+      });
+    } else {
+      saveUser(res, req.user);
+    }
   }
- )
+);
 
 router.post("/register", function(req, res) {
   let { errors, isValid } = registerValidation(req.body);
